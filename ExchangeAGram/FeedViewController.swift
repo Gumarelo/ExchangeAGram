73b2269c5a693d048f9cd189fe5a5a12f1685ee7
Jorge Casariego <jorgekasa@gmail.com>
2015-06-01 17:05:55 -0400

    First app

---
+++ ExchangeAGram/FeedViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FeedViewController.swift
+//  ExchangeAGram
+//
+//  Created by Jorge Casariego on 1/6/15.
+//  Copyright (c) 2015 Enterprise Solutions. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    //UICollectionViewDatasource
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
+}
